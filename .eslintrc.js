66eb310d3ab9d013b8a865377692850851ab21ac
Vincent Comby <vincent@skimlinks.com>
2020-10-17 00:40:44 +0200

    Add eslint config at root of project.
    Eject tsdx eslint config to the root of the project so it can be picked up by VSCode eslint extension.

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+/**
+ * This file has been generated using tsdx command `yarn lint --write-file`.
+ * The config has been ejected so it can be picked up by VSCode ESlint extension.
+ */
+ module.exports = {
+    "extends": [
+      /** CUSTOMIZED RULES */
+      // Add your custom rules here.
+  
+      /** END CUSTOMIZED RULES */
+      'react-app',
+      "prettier/@typescript-eslint",
+      "plugin:prettier/recommended"
+    ]
+  }
