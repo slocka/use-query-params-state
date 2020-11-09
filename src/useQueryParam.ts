6bf154fc7fc39ee9d3911988b4f2e6206e02ffae
Vincent Comby <vincent@skimlinks.com>
2020-11-09 09:47:14 +0100

    Improve TS types for useQueryParam

--- src/useQueryParam.ts
+++ src/useQueryParam.ts
@@ -1,15 +1,31 @@
 import { useCallback } from 'react';
+import { QPARAMS } from './qparams';
 
 import { QueryParamDef } from './queryParamDef';
 
 import { useQueryParamsState } from './useQueryParamsState';
-import { QPARAMS } from './qparams';
+
+/**
+ * Create function overload to handle the default value queryParamDef = QPARAMS.string()
+ * as typescript doesn't seem to allow "queryParamDef: QueryParamDef<T> = QPARAMS.string()"
+ */
+export function useQueryParam(
+  paramName: string,
+  queryParamDef?: undefined,
+  contextData?: any
+): [string | null | undefined, (value?: string | null | undefined) => void];
+
+export function useQueryParam<T>(
+  paramName: string,
+  queryParamDef: QueryParamDef<T>,
+  contextData?: any
+): [T | null | undefined, (value?: T | null | undefined) => void];
 
 export function useQueryParam(
   paramName: string,
   queryParamDef: QueryParamDef<any> = QPARAMS.string(),
   contextData?: any
-) {
+): [any | null | undefined, (value?: any | null | undefined) => void] {
   const [params, setParams] = useQueryParamsState(
     {
       [paramName]: queryParamDef,
@@ -18,11 +34,12 @@ export function useQueryParam(
   );
 
   const setSingleParam = useCallback(
-    value => {
+    (value?: any | null) => {
       setParams({ [paramName]: value });
     },
     [setParams, paramName]
   );
+  const queryParam = params[paramName];
 
-  return [params[paramName], setSingleParam];
+  return [queryParam, setSingleParam];
 }
