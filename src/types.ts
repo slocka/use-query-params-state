3e84f5fb85723e94ded70943fbefe80fd7183f77
Vincent Comby <vincent@skimlinks.com>
2020-11-09 08:51:15 +0100

    Remove unused type

--- src/types.ts
+++ src/types.ts
@@ -49,7 +49,5 @@ export type Serializer<T> = {
   fromUrl: SerializerFromUrlFunction<T>;
 };
 
-export type ParamTypeToSerializerMap = Record<string, Serializer<any>>;
-
 export type DefaultValueFunction<T> = (context?: any) => T;
 export type DefaultValue<T> = T | DefaultValueFunction<T> | undefined | null;
